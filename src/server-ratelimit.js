9c1e1533b90c45aba787aea2763803f82bda08a6
Amanda Munhoz Crozariol <158174011+MandyCro@users.noreply.github.com>
2025-04-04 01:41:59 +0000

    exercicio2

--- src/server-ratelimit.js
+++ src/server-ratelimit.js
@@ -1,58 +1,56 @@
-const express = require('express');
-const rateLimit = require('express-rate-limit');
+import express from 'express'
+import rateLimit from 'express-rate-limit'
 
-const app = express();
-const port = 8081;
+const app = express()
+const port = 8080
 
 // Middleware de rate limiting (Limite de 5 requisições por minuto)
 const limiter = rateLimit({
-    windowMs: 60 * 1000,  // 1 minuto
-    max: 100,  // Limite de 5 requisições
-    message: 'Você excedeu o limite de requisições, tente novamente mais tarde.',
-});
-
-
+  windowMs: 60 * 1000, // 1 minuto
+  max: 100, // Limite de 100 requisições
+  message: 'Você excedeu o limite de requisições, tente novamente mais tarde.',
+})
 
 // Aplica o rate limiter para todas as rotas
-app.use(limiter);
-
+app.use(limiter)
 
 // Função simulando chamada externa
 async function externalService() {
-    return 'Resposta da chamada externa';
+  return 'Resposta da chamada externa'
 }
 
 // Rota que faz a chamada simulada
 app.get('/api/ratelimit', async (req, res) => {
-    try {
-        const result = await externalService();
-        res.send(result);
-    } catch (error) {
-        res.status(500).send(`Erro: ${error.message}`);
-    }
-});
+  try {
+    const result = await externalService()
+    res.send(result)
+  } catch (error) {
+    // @ts-expect-error error is not typed
+    res.status(500).send(`Erro: ${error.message}`)
+  }
+})
 
 // Iniciando o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-});
+  console.log(`Servidor rodando em http://localhost:${port}`)
+})
 
-async function simulateRateLimit() {
-    const url = 'http://localhost:8081/api/ratelimit';
-    const requests = 110; // Número de requisições para exceder o limite (100 no rate limiter)
+// Função que chama 110 vezes o endpoint /api/ratelimit a cada 100ms
+async function breakRateLimit() {
+  let count = 0
 
-    for (let i = 1; i <= requests; i++) {
-        try {
-            const response = await fetch(url)
-            console.log(`Requisição ${i}:`, response.data);
-        } catch (error) {
-            if (error.response && error.response.status === 429) {
-                console.error(`Requisição ${i}: Erro de Rate Limit - ${error.response.data}`);
-            } else {
-                console.error(`Requisição ${i}: Erro inesperado - ${error.message}`);
-            }
-        }
+  const interval = setInterval(async () => {
+    const result = await fetch('http://localhost:8080/api/ratelimit')
+    const data = await result.text()
+
+    console.log(`Count: ${count} // Data: ${data}`)
+
+    count++
+
+    if (count > 110) {
+      clearInterval(interval)
     }
+  }, 100)
 }
 
-simulateRateLimit();
+await breakRateLimit()
